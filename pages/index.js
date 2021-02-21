8aba51ab29214d27dce874b0e647ca1796405262
cesar puentes <caps2695@gmail.com>
2021-02-20 23:52:45 -0500

    🎨  the previous structure is deleted and the new one is added with next

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div>Welcome to Gitreaders!</div>;
+};
+
+export default HomePage;
---
+++ pages/login/index.js
@@ -0,0 +1,7 @@
+import styles from './login.module.scss';
+
+const Login = () => {
+  return <div className={styles.red}>Login</div>;
+};
+
+export default Login;
