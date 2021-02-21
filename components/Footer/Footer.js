4c057cbca7f068b578ec6f0a79aee6fbe80709ea
cesar puentes <caps2695@gmail.com>
2021-02-21 15:50:13 -0500

    🚧 create footer layout

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import Image from 'next/image';
 
+import styles from './footer.module.scss';
+
 const Footer = () => {
   return (
-    <>
-      <Image
-        src='/images/grit-readers.png'
-        alt='Logo'
-        width={100}
-        height={100}
-      />
-      <h3>
-        Platzi Master
-        <br />
-        2021
-      </h3>
-      <h3>With love by Israel's Team</h3>
-    </>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <Image
+          src='/images/grit-readers.png'
+          alt='Logo'
+          width={50}
+          height={50}
+        />
+        <h3>
+          Platzi Master
+          <br />
+          2021
+        </h3>
+        <h3>With love by Israel's Team</h3>
+      </div>
+    </footer>
   );
 };
 
