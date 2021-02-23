6275b2d818ab0c439093ec56deaa1e5f86fe2123
Julian Cubillos <juliandavid25@hotmail.com>
2021-02-23 15:35:56 -0500

    Feature login layout

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,7 +1,18 @@
+import Link from 'next/link' 
 import styles from './login.module.scss';
 
 const Login = () => {
-  return <div className={styles.red}>Login</div>;
+  return <div className={styles.login}>
+      <div className={styles.login__main}>
+        <h1>Bienvenido a GritReaders</h1>
+        <input placeholder="Usuario"></input>
+        <input placeholder="Contraseña"></input>
+        <div>
+          <Link href="/login"><a>Olvide la contraseña</a></Link>
+          <button>entrar</button>
+        </div>
+      </div>
+    </div>;
 };
 
 export default Login;
