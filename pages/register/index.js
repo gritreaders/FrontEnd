48b8cac3f93027789703c22ff458ceeba6ee58c3
Julian Cubillos <juliandavid25@hotmail.com>
2021-02-25 12:43:51 -0500

    Fix, layout register view

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,7 +1,9 @@
+import Link from 'next/link';
 import styles from './register.module.scss';
 
 const Register = () => {
   return <div className={styles.register}>
+      <Link href="/"><a className={styles.register__logo}></a></Link>
       <div className={styles.register__main}>
         <h1>Registrese a GritReaders</h1>
         <input placeholder="Name"></input>
