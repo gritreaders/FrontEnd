139611a3f10272a57e7a8f40a657bbfddd209c5c
Julian Cubillos <juliandavid25@hotmail.com>
2021-02-25 20:45:19 -0500

    Feat Component Layout StarRank without functionality

--- components/StarsRank/StarsRank.js
+++ components/StarsRank/StarsRank.js
@@ -4,9 +4,17 @@ import styles from './starsRank.module.scss';
 const StarsRank = () => {
   return (
     <section>
-      <div className={styles.container}>1</div>
+      <div className={styles.container}>
+        <a href='#' value="1">★</a>
+        <a href='#' value="2">★</a>
+        <a href='#' value="3">★</a>
+        <a href='#' value="4">★</a>
+        <a href='#' value="5">★</a>
+      </div>
     </section>
   );
 };
 
 export default StarsRank;
+
+//<a href='#'>★</a> &#9733;  
