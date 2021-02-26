ce400d5c2b9776b99406b01c4b00a557745a866b
cesar puentes <caps2695@gmail.com>
2021-02-25 20:56:37 -0500

    🚧 working component search

---
+++ components/Search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './search.module.scss'
+
+const Search = () => {
+  return (
+    <div className={styles.container}>
+      <input type="text"/>
+      <img src="" alt="icono"/>
+    </div>
+  )
+}
+
+export default Search
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '../components/Card/Card';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
+import Search from '../components/Search/Search';
 import styles from './home.module.scss'
 
 const HomePage = () => {
@@ -9,6 +10,9 @@ const HomePage = () => {
     <>
       <Header />
       <section className={styles.main}>
+        <div className={styles.search}>
+          <Search />
+        </div>
         <div className={styles.cardList}>
           <Card />
           <Card />
